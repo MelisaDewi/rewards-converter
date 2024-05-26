49f8e94d4029d1930cc53a2be55eafb34bcf5729
MelisaDewi <105781889+MelisaDewi@users.noreply.github.com>
2024-05-26 20:34:36 +0700

    Initial commit

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,32 @@
+public class RewardValue {
+
+    private double cash;
+    private int miles;
+    private static final double CONVERTER = 0.0035;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+        this.miles = (int) cashtoMiles(cash);
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+        this.cash = milestoCash(miles);
+    }
+
+    private double cashtoMiles(double cash){
+        return cash/CONVERTER;
+    }
+
+    private double milestoCash(int miles){
+        return miles*CONVERTER;
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return miles;
+    }
+}
